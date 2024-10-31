49cebc67968c491cf2426e3d357b84a7bc588319
Adrian Estañ Gonzalez <adrianestan1005@gmail.com>
2024-10-31 11:11:44 +0100

    Validación dto create finalizado

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,5 +1,9 @@
+import { IsDate, IsString } from 'class-validator';
+
 export class CreateUsuarioDto {
+  @IsString()
   name: string;
+  @IsDate()
   fechaNacimiento: Date;
 
   constructor(name: string, fechaNacimiento: Date) {
